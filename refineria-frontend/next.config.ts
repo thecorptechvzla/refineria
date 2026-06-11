2ddd8dee6983f73361672fece67a647f2b0c1c71
CATheCorp <cabache@thecorptech.com>
2026-06-11 18:49:37 -0400

    fix cors 1

--- refineria-frontend/next.config.ts
+++ refineria-frontend/next.config.ts
@@ -1,14 +1,20 @@
 import type { NextConfig } from "next";
 
+const NEXT_PUBLIC_API_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:4000' // O tu IP local si pruebas desde el teléfono: 'http://192.168.0.106:4000'
+  : 'https://refineria-backend.vercel.app';
+
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    '192.168.0.106',
+    '192.168.0.106', // Mantenlo si desarrollas desde red local
   ],
   async rewrites() {
     return [
       {
+        // Cada vez que NextJS vea una ruta que empiece con /api/...
         source: '/api/:path*',
-        destination: 'http://localhost:4000/:path*',
+        // La redirigirá de forma transparente al backend correcto sin levantar sospechas de CORS
+        destination: `${NEXT_PUBLIC_API_URL}/:path*`, 
       },
     ];
   },
